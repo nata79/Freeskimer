db744659ba957918414d85b867ecbdec09dae7a4
André Barbosa <albmail88@gmail.com>
2013-03-23 12:30:35 +0000

    added option to pass a block that executes before setting the value

--- lib/freeskimer/model_additions.rb
+++ lib/freeskimer/model_additions.rb
@@ -2,17 +2,18 @@ module Freeskimer
   module ModelAdditions
      def freeskim(att, options)
        
-       # Validates arguments
-       raise "'to' attribitute is missing" unless options[:to]
+        # Validates arguments
+        raise "'to' attribitute is missing" unless options[:to]
        
-       # Define get method
-       send :define_method, att do
-         tmp = JSON.parse(send(options[:to]).to_s) if send(options[:to])
-         tmp[att.to_s] if tmp and tmp[att.to_s]
-       end
+        # Define get method
+        send :define_method, att do
+          tmp = JSON.parse(send(options[:to]).to_s) if send(options[:to])
+          tmp[att.to_s] if tmp and tmp[att.to_s]
+        end
        
-       # Define set method
+        # Define set method
         send :define_method, "#{att}=" do |value|
+          value = yield(value) if block_given?
           tmp = {}
           tmp = JSON.parse send options[:to] if send options[:to]
           tmp[att] = value
